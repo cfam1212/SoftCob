8795109f3e2c507121d51e3f99829bffb1fb64e6
erick alvear <erick7alvear@outlook.es>
2021-04-04 17:06:39 -0500

    breanch_gestiones

---
+++ SoftCob/Views/Breanch/WFrm_BrenchGestiones.aspx.cs
@@ -0,0 +1,44 @@
+﻿
+
+namespace SoftCob.Views.Breanch
+{
+    using ControllerSoftCob;
+    using System;
+    using System.Configuration;
+    using System.Data;
+    using System.Web.UI;
+    public partial class WFrm_BrenchGestiones : Page
+    {
+        #region Variables
+        DataSet _dts = new DataSet();
+        #endregion
+
+        #region Load
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ViewState["CodigoCEDE"] = Request["CodigoCEDE"];
+                ViewState["CodigoCPCE"] = Request["CodigoCPCE"];
+                ViewState["CodigoGEST"] = Request["CodigoGEST"];
+                ViewState["Operacion"] = Request["Operacion"];
+                ViewState["Conectar"] = ConfigurationManager.AppSettings["SqlConn"];
+                Lbltitulo.Text = "Gestiones Realizadas";
+                FunCargarDatos();
+            }
+        }
+        #endregion
+
+        #region Procedimientos y Funciones
+        private void FunCargarDatos()
+        {
+            _dts = new ConsultaDatosDAO().FunConsultaDatos(223, int.Parse(ViewState["CodigoCPCE"].ToString()),
+                int.Parse(ViewState["CodigoGEST"].ToString()), 0, "", ViewState["Operacion"].ToString(), "",
+                ViewState["Conectar"].ToString());
+
+            GrdvGestiones.DataSource = _dts;
+            GrdvGestiones.DataBind();
+        }
+        #endregion
+    }
+}
